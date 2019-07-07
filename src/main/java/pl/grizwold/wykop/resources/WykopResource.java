c08a16bd766ca0526ff70bb68f44e70d38846299
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-07 12:19:21 +0200

    defining base api url

---
+++ src/main/java/pl/grizwold/wykop/resources/WykopResource.java
@@ -0,0 +1,9 @@
+package pl.grizwold.wykop.resources;
+
+public class WykopResource {
+    protected static String baseUrl = "https://a2.wykop.pl";
+
+    public static void setBaseUrl(String baseUrl) {
+        WykopResource.baseUrl = baseUrl;
+    }
+}
