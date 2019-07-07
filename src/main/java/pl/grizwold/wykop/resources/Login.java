95c151b681265a653d64ff7cf760064642a93903
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-07 13:27:15 +0200

    removed callable interface from login

--- src/main/java/pl/grizwold/wykop/resources/Login.java
+++ src/main/java/pl/grizwold/wykop/resources/Login.java
@@ -7,21 +7,18 @@ import pl.grizwold.wykop.model.WykopRequest;
 import pl.grizwold.wykop.model.WykopResponse;
 
 import java.io.IOException;
-import java.util.concurrent.Callable;
 
-public class Login extends WykopResource implements Callable<WykopResponse> {
+public class Login extends WykopResource {
     private static final String ACCOUNT_KEY = "accountkey";
     private static final String USER_KEY = "userkey";
     private static final String DATA_NODE = "data";
-    private final WykopClient client;
     private final String accountKey;
 
     public Login(WykopClient client, String accountKey) {
-        this.client = client;
+        super(client);
         this.accountKey = accountKey;
     }
 
-    @Override
     public WykopResponse call() throws IOException {
         WykopRequest request = this.toRequest();
 
