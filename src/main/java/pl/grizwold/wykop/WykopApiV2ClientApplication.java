4848948b141853c79c0f0db1792a9e849ef9f3fc
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-07 12:19:00 +0200

    example usages

--- src/main/java/pl/grizwold/wykop/WykopApiV2ClientApplication.java
+++ src/main/java/pl/grizwold/wykop/WykopApiV2ClientApplication.java
@@ -9,13 +9,22 @@ import java.io.IOException;
 public class WykopApiV2ClientApplication {
 
     public static void main(String[] args) throws IOException {
-//        execute(new HttpGet("https://a2.wykop.pl/Entries/Entry/42463679/"));
+        callingPostEndpoint();
+    }
 
+    public static void definingGlobalParams() throws IOException {
         WykopRequest wykopRequest = new WykopRequest("https://a2.wykop.pl/Entries/Entry/42463679/");
 
-//        WykopRequest wykopRequest = new WykopRequest("https://a2.wykop.pl/Login/Index/")
-//                .addPostParam("accountkey", "KkO9fA3o2uRviR4CVfVn")
-//                .addParam("appkey", "kUhKLnGypc");
+        WykopClient wykopClient = new WykopClient("kUhKLnGypc", "vbrbQmciBU");
+        wykopClient.set(ApiParam.OUTPUT_CLEAR);
+
+        WykopResponse wykopResponse = wykopClient.execute(wykopRequest);
+        System.out.println(wykopResponse.toString());
+    }
+
+    public static void callingPostEndpoint() throws IOException {
+        WykopRequest wykopRequest = new WykopRequest("https://a2.wykop.pl/Login/Index/")
+                .addPostParam("accountkey", "KkO9fA3o2uRviR4CVfVn");
 
         WykopClient wykopClient = new WykopClient("kUhKLnGypc", "vbrbQmciBU");
         wykopClient.set(ApiParam.OUTPUT_CLEAR);
