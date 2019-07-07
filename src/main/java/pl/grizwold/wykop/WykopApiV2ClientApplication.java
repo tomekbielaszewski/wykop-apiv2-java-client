78ae4a270a10a8675790b38502b21a9790fe8180
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-07 09:57:03 +0200

    wykop client usage

--- src/main/java/pl/grizwold/wykop/WykopApiV2ClientApplication.java
+++ src/main/java/pl/grizwold/wykop/WykopApiV2ClientApplication.java
@@ -1,22 +1,11 @@
 package pl.grizwold.wykop;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import pl.grizwold.wykop.api.Signer;
-import pl.grizwold.wykop.model.ApiKey;
 import pl.grizwold.wykop.model.WykopRequest;
 import pl.grizwold.wykop.model.WykopResponse;
 
 import java.io.IOException;
 
 public class WykopApiV2ClientApplication {
-    private static CloseableHttpClient client = HttpClients.createDefault();
-    private static Signer signer = new Signer(new ApiKey("kUhKLnGypc", "vbrbQmciBU"));
-    private static ObjectMapper om = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
 //        execute(new HttpGet("https://a2.wykop.pl/Entries/Entry/42463679/"));
@@ -25,29 +14,9 @@ public class WykopApiV2ClientApplication {
                 .addPostParam("accountkey", "KkO9fA3o2uRviR4CVfVn")
                 .addParam("appkey", "kUhKLnGypc");
 
-        String signature = signer.getSignature(wykopRequest.getUrl(), wykopRequest.getPayloadValues());
-        wykopRequest.sign(signature);
+        WykopClient wykopClient = new WykopClient("kUhKLnGypc", "vbrbQmciBU");
 
-        CloseableHttpResponse closeableHttpResponse = client.execute(wykopRequest.toRequest());
-        System.out.println(closeableHttpResponse.getStatusLine().toString());
-
-        WykopResponse wykopResponse = getWykopResponse(closeableHttpResponse);
+        WykopResponse wykopResponse = wykopClient.execute(wykopRequest);
         System.out.println(wykopResponse.toString());
-    }
-
-    private static WykopResponse getWykopResponse(CloseableHttpResponse content) throws IOException {
-        String json = EntityUtils.toString(content.getEntity());
-        System.out.println(json);
-
-        JsonNode jsonNode = om.readTree(json);
-
-        WykopResponse.Error error = null;
-        if (jsonNode.has("error")) {
-            JsonNode errorNode = jsonNode.get("error");
-            int code = errorNode.get("code").asInt();
-            String message_pl = errorNode.get("message_pl").asText();
-            error = new WykopResponse.Error(code, message_pl);
-        }
-        return new WykopResponse(json, error);
     }
 }
